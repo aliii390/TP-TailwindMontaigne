e744349ee76f0d178bb19a4f9bf46eb615d9674e
aliii390 <alli.said.errahmani@gmail.com>
2024-10-31 16:32:36 +0100

    ADD: debut du header 2eme maquette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        neutral:{
+          'off-white': 'hsl(36, 100%, 99%)',
+      },
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+        Rokkitt: ["Rokkitt", "sans-serif"],
+      },
+      backgroundImage: {
+        'hero-img': "url('../images/bg_header.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
 
